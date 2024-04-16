9678581636cfd5cf57f8f8f2a8bf847f15597900
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-16 11:24:22 -0400

    implementando vídeo local

--- src/components/Institutional.jsx
+++ src/components/Institutional.jsx
@@ -1,11 +1,11 @@
 // import React from 'react';
 
-export function Institutional(){
+export function Institutional() {
     return (
         <div className='w-full bg-white py-12 px-4 flex justify-center items-center'>
             <div className='max-w-[914px] flex flex-col justify-between items-center'>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold mb-4'>Institucional</h1>
-                <iframe
+                {/* <iframe
                     width="700"
                     height="412"
                     src="https://www.youtube.com/embed/eIB1rA2k4IA"
@@ -14,7 +14,14 @@ export function Institutional(){
                     referrerPolicy="strict-origin-when-cross-origin"
                     allowFullScreen
                     className='rounded-md md:w-[750px] w-full h-[450px]'
-                />
+                /> */}
+                <video
+                    width="700"
+                    height="412"
+                    controls
+                    className="rounded-md md:w-[750px] w-full h-[450px]">
+                    <source src="src/assets/video/institucional.mp4" type="video/mp4" />
+                </video>
             </div>
         </div>
     );
